e59b87caf3823dc7665bd0ac42335d911f20c10e
Hedi zouaoui <hedi.zouaoui@istic.ucar.tn>
2023-04-09 02:09:05 +0100

    first commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,14 +1,9 @@
 package com.example.demo;
 
-import java.util.List;
-
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
-import com.example.demo.customer.Customer;
-import com.example.demo.customer.CustomerRepository;
+
 
 @SpringBootApplication
 
